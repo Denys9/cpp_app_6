acc2fdd7b10636d6c7f42b645137d0ea54aa90ab
lozin <lozindenis6@gmail.com>
2023-01-24 20:45:50 +0200

    №3

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
- 
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    const int size = 5;
+    int arr2D[size][size];
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            arr2D[i][j] = rand() % 10;
+            cout << arr2D[i][j] << ' ';
+        }
+        cout << endl;
+    }
